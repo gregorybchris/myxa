8e5dad8561840051ccea4c94f6ef95a7e9eadad9
Chris Gregory <gregorybchris@gmail.com>
2024-12-27 02:48:33 -0800

    Add manager test stubs

---
+++ tests/test_manager.py
@@ -0,0 +1,33 @@
+class TestManager:
+    def test_print_package_info_without_lock_raises_user_error(self) -> None:
+        pass
+
+    def test_publish_without_lock_raises_user_error(self) -> None:
+        pass
+
+    def test_publish_duplicate_version_raises_user_error(self) -> None:
+        pass
+
+    def test_package_not_found_in_indexes_raises_user_error(self) -> None:
+        pass
+
+    def test_add_existing_dependency_raises_user_error(self) -> None:
+        pass
+
+    def test_publish_adds_package_to_index(self) -> None:
+        pass
+
+    def test_lock_updates_lock_deps(self) -> None:
+        pass
+
+    def test_save_package_to_file(self) -> None:
+        pass
+
+    def test_load_package_from_file(self) -> None:
+        pass
+
+    def test_save_index_to_file(self) -> None:
+        pass
+
+    def test_load_index_from_file(self) -> None:
+        pass
