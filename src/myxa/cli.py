82c5fb0fdc7db4040350a5f1f09b42137f78f086
Chris Gregory <gregorybchris@gmail.com>
2025-03-31 00:07:17 -0700

    Make CLI context for errors and logging

--- src/myxa/cli.py
+++ src/myxa/cli.py
@@ -1,7 +1,10 @@
+from __future__ import annotations
+
 import logging
 import os
 import tempfile
 from contextlib import contextmanager
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Annotated, Generator, Optional
 
@@ -55,14 +58,23 @@ def save_index(manager: Manager, index: Index) -> None:
     manager.save_index(index, index_filepath)
 
 
-@contextmanager
-def error_handler(manager: Manager, debug: bool = False) -> Generator[None, None, None]:
-    try:
-        yield
-    except UserError as exc:
-        manager.printer.print_error(str(exc))
-        if debug:
-            raise exc
+@dataclass(kw_only=True)
+class CliContext:
+    index: Index
+    manager: Manager
+
+    @classmethod
+    @contextmanager
+    def context(cls, info: bool = False, debug: bool = False) -> Generator[CliContext, None, None]:
+        set_logger_config(info, debug)
+        manager = Manager()
+        index = load_index(manager)
+        try:
+            yield CliContext(index=index, manager=manager)
+        except UserError as exc:
+            manager.printer.print_error(str(exc))
+            if debug:
+                raise exc
 
 
 @app.command(help="Initialize a new package")
@@ -72,31 +84,26 @@ def init(
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    set_logger_config(info, debug)
-    manager = Manager()
-    with error_handler(manager, debug=debug):
-        manager.init(DEFAULT_PACKAGE_FILEPATH, name=name, description=description)
+    with CliContext.context(info=info, debug=debug) as context:
+        context.manager.init(DEFAULT_PACKAGE_FILEPATH, name=name, description=description)
 
 
 @app.command(help="Print information about the package")
 def info(  # noqa: PLR0913
-    version: Annotated[Optional[str], typer.Option("--version")] = None,
+    version_str: Annotated[Optional[str], typer.Option("--version")] = None,
     show_dependencies: Annotated[bool, typer.Option("--show-deps/--no-deps")] = True,
     show_lock: Annotated[bool, typer.Option("--show-lock/--no-lock")] = True,
     show_members: Annotated[bool, typer.Option("--show-members/--no-members")] = True,
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    set_logger_config(info, debug)
-    manager = Manager()
-    with error_handler(manager, debug=debug):
-        package = manager.load_package(DEFAULT_PACKAGE_FILEPATH)
-        index = load_index(manager)
-        version_obj = Version.new(version) if version else None
-        manager.info(
+    with CliContext.context(info=info, debug=debug) as context:
+        package = context.manager.load_package(DEFAULT_PACKAGE_FILEPATH)
+        version = Version.new(version_str) if version_str else None
+        context.manager.info(
             package,
-            index,
-            version=version_obj,
+            context.index,
+            version=version,
             show_dependencies=show_dependencies,
             show_lock=show_lock,
             show_members=show_members,
@@ -110,18 +117,15 @@ app.command(name="show", help="Print information about the package")(info)
 @app.command(help="Add a dependency to the package")
 def add(
     dep_name: Annotated[str, typer.Argument(...)],
-    version: Annotated[Optional[str], typer.Option("--version")] = None,
+    version_str: Annotated[Optional[str], typer.Option("--version")] = None,
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    set_logger_config(info, debug)
-    manager = Manager()
-    with error_handler(manager, debug=debug):
-        package = manager.load_package(DEFAULT_PACKAGE_FILEPATH)
-        index = load_index(manager)
-        version_obj = Version.new(version) if version else None
-        manager.add(package, dep_name, index, version=version_obj)
-        manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
+    with CliContext.context(info=info, debug=debug) as context:
+        package = context.manager.load_package(DEFAULT_PACKAGE_FILEPATH)
+        version = Version.new(version_str) if version_str else None
+        context.manager.add(package, dep_name, context.index, version=version)
+        context.manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
 
 
 @app.command(help="Remove a dependency from the package")
@@ -130,12 +134,10 @@ def remove(
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    set_logger_config(info, debug)
-    manager = Manager()
-    with error_handler(manager, debug=debug):
-        package = manager.load_package(DEFAULT_PACKAGE_FILEPATH)
-        manager.remove(package, dep_name)
-        manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
+    with CliContext.context(info=info, debug=debug) as context:
+        package = context.manager.load_package(DEFAULT_PACKAGE_FILEPATH)
+        context.manager.remove(package, dep_name)
+        context.manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
 
 
 @app.command(help="Lock the package dependencies")
@@ -143,13 +145,10 @@ def lock(
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    set_logger_config(info, debug)
-    manager = Manager()
-    with error_handler(manager, debug=debug):
-        package = manager.load_package(DEFAULT_PACKAGE_FILEPATH)
-        index = load_index(manager)
-        manager.lock(package, index)
-        manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
+    with CliContext.context(info=info, debug=debug) as context:
+        package = context.manager.load_package(DEFAULT_PACKAGE_FILEPATH)
+        context.manager.lock(package, context.index)
+        context.manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
 
 
 @app.command(help="Unlock the package dependencies", hidden=True)
@@ -157,13 +156,11 @@ def unlock(
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    set_logger_config(info, debug)
-    manager = Manager()
-    manager.printer.print_warning("warning: unlock is not a supported Myxa command!")
-    with error_handler(manager, debug=debug):
-        package = manager.load_package(DEFAULT_PACKAGE_FILEPATH)
-        manager.unlock(package)
-        manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
+    with CliContext.context(info=info, debug=debug) as context:
+        context.manager.printer.print_warning("warning: unlock is not a supported Myxa command!")
+        package = context.manager.load_package(DEFAULT_PACKAGE_FILEPATH)
+        context.manager.unlock(package)
+        context.manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
 
 
 @app.command(help="Update all dependencies to the latest compatible version")
@@ -171,43 +168,34 @@ def update(
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    set_logger_config(info, debug)
-    manager = Manager()
-    with error_handler(manager, debug=debug):
-        package = manager.load_package(DEFAULT_PACKAGE_FILEPATH)
-        index = load_index(manager)
-        manager.update(package, index)
-        manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
+    with CliContext.context(info=info, debug=debug) as context:
+        package = context.manager.load_package(DEFAULT_PACKAGE_FILEPATH)
+        context.manager.update(package, context.index)
+        context.manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
 
 
 @app.command(help="Check if there are breaking changes")
 def check(
-    version: Annotated[Optional[str], typer.Option("--version")] = None,
+    version_str: Annotated[Optional[str], typer.Option("--version")] = None,
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    set_logger_config(info, debug)
-    manager = Manager()
-    with error_handler(manager, debug=debug):
-        package = manager.load_package(DEFAULT_PACKAGE_FILEPATH)
-        index = load_index(manager)
-        version_obj = Version.new(version) if version else None
-        manager.check(package, index, version=version_obj)
+    with CliContext.context(info=info, debug=debug) as context:
+        package = context.manager.load_package(DEFAULT_PACKAGE_FILEPATH)
+        version = Version.new(version_str) if version_str else None
+        context.manager.check(package, context.index, version=version)
 
 
 @app.command(help="Check if there are changes in the package")
 def diff(
-    version: Annotated[Optional[str], typer.Option("--version")] = None,
+    version_str: Annotated[Optional[str], typer.Option("--version")] = None,
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    set_logger_config(info, debug)
-    manager = Manager()
-    with error_handler(manager, debug=debug):
-        package = manager.load_package(DEFAULT_PACKAGE_FILEPATH)
-        index = load_index(manager)
-        version_obj = Version.new(version) if version else None
-        manager.diff(package, index, version=version_obj)
+    with CliContext.context(info=info, debug=debug) as context:
+        package = context.manager.load_package(DEFAULT_PACKAGE_FILEPATH)
+        version = Version.new(version_str) if version_str else None
+        context.manager.diff(package, context.index, version=version)
 
 
 @app.command(help="Publish the current package to the index")
@@ -217,32 +205,26 @@ def publish(
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    set_logger_config(info, debug)
-    manager = Manager()
-    with error_handler(manager, debug=debug):
-        package = manager.load_package(DEFAULT_PACKAGE_FILEPATH)
-        index = load_index(manager)
-        manager.publish(package, index, interactive=interactive, major=major)
-        save_index(manager, index)
-        manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
+    with CliContext.context(info=info, debug=debug) as context:
+        package = context.manager.load_package(DEFAULT_PACKAGE_FILEPATH)
+        context.manager.publish(package, context.index, interactive=interactive, major=major)
+        save_index(context.manager, context.index)
+        context.manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
 
 
 @app.command(help="Yank the current package from the index", hidden=True)
 def yank(
-    version: Annotated[str, typer.Argument(...)],
+    version_str: Annotated[str, typer.Argument(...)],
     interactive: Annotated[bool, typer.Option("--interactive/--no-interactive")] = True,
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    version_obj = Version.new(version)
-    set_logger_config(info, debug)
-    manager = Manager()
-    manager.printer.print_warning("warning: yank is not a supported Myxa command!")
-    with error_handler(manager, debug=debug):
-        package = manager.load_package(DEFAULT_PACKAGE_FILEPATH)
-        index = load_index(manager)
-        manager.yank(package, version_obj, index, interactive=interactive)
-        save_index(manager, index)
+    with CliContext.context(info=info, debug=debug) as context:
+        context.manager.printer.print_warning("warning: yank is not a supported Myxa command!")
+        package = context.manager.load_package(DEFAULT_PACKAGE_FILEPATH)
+        version = Version.new(version_str)
+        context.manager.yank(package, version, context.index, interactive=interactive)
+        save_index(context.manager, context.index)
 
 
 @app.command(help="List all packages in the index")
@@ -252,24 +234,19 @@ def index(
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    set_logger_config(info, debug)
-    manager = Manager()
-    with error_handler(manager, debug=debug):
-        index = load_index(manager)
-        manager.printer.print_index(index, package_name=package_name, show_versions=show_versions)
+    with CliContext.context(info=info, debug=debug) as context:
+        context.manager.printer.print_index(context.index, package_name=package_name, show_versions=show_versions)
 
 
 @app.command(help="Set the version of the package", hidden=True)
 def version(
-    version: Annotated[str, typer.Argument(...)],
+    version_str: Annotated[str, typer.Argument(...)],
     info: Annotated[bool, typer.Option("--info/--no-info")] = False,
     debug: Annotated[bool, typer.Option("--debug/--no-debug")] = False,
 ) -> None:
-    set_logger_config(info, debug)
-    manager = Manager()
-    manager.printer.print_warning("version is not a supported Myxa command!")
-    with error_handler(manager, debug=debug):
-        package = manager.load_package(DEFAULT_PACKAGE_FILEPATH)
-        version_obj = Version.new(version)
-        manager.set_version(package, version_obj)
-        manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
+    with CliContext.context(info=info, debug=debug) as context:
+        context.manager.printer.print_warning("version is not a supported Myxa command!")
+        package = context.manager.load_package(DEFAULT_PACKAGE_FILEPATH)
+        version = Version.new(version_str)
+        context.manager.set_version(package, version)
+        context.manager.save_package(package, DEFAULT_PACKAGE_FILEPATH)
