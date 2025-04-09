23f0794355bd9ed7a6f883e124d8e6650d84db46
Chris Gregory <gregorybchris@gmail.com>
2025-04-09 02:15:29 -0700

    Split up version tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,17 +1,10 @@
 import pytest
-import semver
 
-from myxa import __version__
 from myxa.errors import UserError
 from myxa.version import Version
 
 
 class TestVersion:
-    def test_version(self) -> None:
-        version = semver.VersionInfo.parse(__version__)
-        assert version.major == 0
-        assert version.minor > 0
-
     def test_version_invalid_from_str_raises_user_error(self) -> None:
         with pytest.raises(UserError, match="Invalid version string: 100"):
             Version.new("100")
